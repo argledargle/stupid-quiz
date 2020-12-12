fa8ddd482e258fda3a39ed39d9972cd471482242
Alex <alexpeter@northwesternmutual.com>
2020-12-12 12:04:09 -0600

    fix: fixes results page

--- src/results/results.js
+++ src/results/results.js
@@ -4,44 +4,47 @@ import { render } from '@testing-library/react'
 
 const results = obj => {
   const [firstInput, secondInput, thirdInput] = obj.userInput
+  console.log(firstInput)
+  console.log(secondInput)
+  console.log(thirdInput)
   const firstAnswer = (firstInput, thirdInput) => {
-    if (firstInput === 0) {
-      switch (thirdInput) {
-        case 0:
-          return 'Nippy'
-        case 1:
-          return 'Figgy'
-        case 2:
-          return 'Muffdust'
-        case 3:
-          return 'Hooker'
-        default:
-          return 'oops'
-      }
-    }
     if (firstInput === 1) {
       switch (thirdInput) {
-        case 0:
-          return 'Flaptoot'
         case 1:
-          return 'Snowball'
+          return 'Nippy'
         case 2:
-          return 'Smooshy'
+          return 'Figgy'
         case 3:
-          return 'Belchie'
+          return 'Muffdust'
+        case 4:
+          return 'Hooker'
         default:
           return 'oops'
       }
     }
     if (firstInput === 2) {
       switch (thirdInput) {
-        case 0:
-          return 'Schnocker'
         case 1:
-          return 'Cocksmack'
+          return 'Flaptoot'
         case 2:
-          return 'Bimbo'
+          return 'Snowball'
         case 3:
+          return 'Smooshy'
+        case 4:
+          return 'Belchie'
+        default:
+          return 'oops'
+      }
+    }
+    if (firstInput === 3) {
+      switch (thirdInput) {
+        case 1:
+          return 'Schnocker'
+        case 2:
+          return 'Cocksmack'
+        case 3:
+          return 'Bimbo'
+        case 4:
           return 'Honky'
         default:
           return 'oops'
@@ -49,49 +52,49 @@ const results = obj => {
     }
   }
   const secondAnswer = (firstInput, secondInput) => {
-    if (secondInput === 0) {
-      switch (firstInput) {
-        case 0:
-          return 'The Motherfuckin'
-        case 1:
-          return 'The Slutty'
-        case 2:
-          return 'The Loose-Lipped'
-        default:
-          return 'oops'
-      }
-    }
     if (secondInput === 1) {
       switch (firstInput) {
-        case 0:
-          return 'The Horny'
         case 1:
-          return 'The Shitfaced'
+          return 'The Motherfuckin'
         case 2:
-          return 'The Red-Assed'
+          return 'The Slutty'
+        case 3:
+          return 'The Loose-Lipped'
         default:
           return 'oops'
       }
     }
     if (secondInput === 2) {
       switch (firstInput) {
-        case 0:
-          return 'Schnocker'
         case 1:
-          return 'Cocksmack'
+          return 'The Horny'
         case 2:
-          return 'Bimbo'
+          return 'The Shitfaced'
+        case 3:
+          return 'The Red-Assed'
         default:
           return 'oops'
       }
     }
     if (secondInput === 3) {
       switch (firstInput) {
-        case 0:
-          return 'The Bootie Shakin'
         case 1:
-          return 'The Big Boobed'
+          return 'Schnocker'
         case 2:
+          return 'Cocksmack'
+        case 3:
+          return 'Bimbo'
+        default:
+          return 'oops'
+      }
+    }
+    if (secondInput === 4) {
+      switch (firstInput) {
+        case 1:
+          return 'The Bootie Shakin'
+        case 2:
+          return 'The Big Boobed'
+        case 3:
           return 'Honky'
         default:
           return 'oops'
@@ -99,43 +102,43 @@ const results = obj => {
     }
   }
   const thirdAnswer = (firstInput, secondInput) => {
-    if (firstInput === 0) {
-      switch (secondInput) {
-        case 0:
-          return 'Crack Whore'
-        case 1:
-          return 'Pecker Wrecker'
-        case 2:
-          return 'Asshole'
-        case 3:
-          return 'Dime Piece'
-        default:
-          return 'oops'
-      }
-    }
     if (firstInput === 1) {
       switch (secondInput) {
-        case 0:
-          return 'Fuck Boi'
         case 1:
-          return 'Dumpster Slut'
+          return 'Crack Whore'
         case 2:
-          return 'Wigger'
+          return 'Pecker Wrecker'
         case 3:
-          return 'Welfare Mom'
+          return 'Asshole'
+        case 4:
+          return 'Dime Piece'
         default:
           return 'oops'
       }
     }
     if (firstInput === 2) {
       switch (secondInput) {
-        case 0:
-          return 'Meth Head'
         case 1:
-          return 'Man Slammer'
+          return 'Fuck Boi'
         case 2:
-          return 'Douche Canoe'
+          return 'Dumpster Slut'
         case 3:
+          return 'Wigger'
+        case 4:
+          return 'Welfare Mom'
+        default:
+          return 'oops'
+      }
+    }
+    if (firstInput === 3) {
+      switch (secondInput) {
+        case 1:
+          return 'Meth Head'
+        case 2:
+          return 'Man Slammer'
+        case 3:
+          return 'Douche Canoe'
+        case 4:
           return 'Wiener Whacker'
         default:
           return 'oops'
